9b9c741590ebfa83ded21926e0cf3ae2611bdfcb
Fazil Khusnutdinov <khusnutdinov.fazil@gmail.com>
2025-04-02 18:43:39 +0200

    proof-of-concept server

--- main.go
+++ main.go
@@ -4,14 +4,20 @@ import (
 	"github.com/golang/protobuf/proto"
 	"log"
 	"net"
+	"os"
 	"tcp-proto-server/proto"
 )
 
+const PORT = ":7000"
+
 func main() {
-	listener, err := net.Listen("tcp", ":8080")
+	listener, err := net.Listen("tcp", PORT)
 	if err != nil {
-		log.Fatalf("ERROR: %v", err)
+		log.Printf("ERROR: could not create tcp socket (%v)\n", err)
+		os.Exit(1)
 	}
+
+	log.Printf("Info: listening on port %s\n", PORT)
 
 	for {
 		if conn, err := listener.Accept(); err == nil {
@@ -21,21 +27,21 @@ func main() {
 }
 
 func handleConnection(conn net.Conn) {
-	log.Printf("new connection from %s", conn.RemoteAddr())
+	log.Printf("Info: new connection from %s\n", conn.RemoteAddr())
 	defer conn.Close()
 
 	msg := data.Data{Value: 1, Timestamp: 0}
 	d, err := proto.Marshal(&msg)
 	if err != nil {
-		log.Printf("ERROR: %v", err)
+		log.Printf("ERROR: could not encode message (%v)\n", err)
 		return
 	}
 
 	length, err := conn.Write(d)
 	if err != nil {
-		log.Printf("ERROR: %v", err)
+		log.Printf("ERROR: could not write to connection (%v)\n", err)
 		return
 	}
 
-	log.Printf("wrote %d bytes", length)
+	log.Printf("Info: wrote %d bytes\n", length)
 }
